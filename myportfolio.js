1f5329e3c9e2199a8a0ed8e77aa494ecd9081b3e
emmydon233 <emmyd3900@gmail.com>
2026-09-09 13:40:01 -0700

    Create myportfolio.js

---
+++ myportfolio.js
@@ -0,0 +1,55 @@
+// // Contact form
+
+// const contactForm = document.querySelector(".contact-form");
+
+// contactForm.addEventListener("submit", function(event) {
+
+//     event.preventDefault();
+
+//     alert("Thank you for your message! I will get back to you soon.");
+
+//     contactForm.reset();
+
+// });
+
+// ================= HAMBURGER MENU =================
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+// Close menu after clicking a link
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
+
+
+// ================= CONTACT FORM =================
+
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Thank you for your message! I will get back to you soon.");
+
+    contactForm.reset();
+
+});
+
